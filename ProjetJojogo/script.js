0de62dba3ee2bd777e7f49ec64c88245e58fc961
yacine bouklif <y.bouklif@it-students.fr>
2024-09-07 19:43:41 +0200

    Ajout seconde page Inscription

--- ProjetJojogo/script.js
+++ ProjetJojogo/script.js
@@ -23,5 +23,49 @@ tailwind.config = {
   
     controleurTheme.addEventListener("change", () => basculerTheme(controleurTheme.checked));
   
-    basculerTheme(localStorage.getItem("modeSombre") === "true");
+    basculerTheme(localStorage.getItem("modeSombre") ===  "true");
   });
+  
+
+
+//   async function ChercherPokemon (){
+
+// let texte = document.getElementById('monTexte')
+// let image = document.getElementById('imagepokemon')
+// let inputElement = document.getElementById('monInput')
+
+//     try {
+
+//       let input = inputElement.value.trim().toLowerCase()
+// const response = await fetch (`https://pokeapi.co/api/v2/pokemon/${input}`)
+
+// if(!response.ok){
+//   throw new Error("Ca n'a pas marché")
+// }
+
+// let data = await response.json()
+// console.log(data)
+// let NomPokemon = data.name
+// let ImagePokemon = data.sprites.front_default
+
+// texte.textContent = NomPokemon
+// image.src = ImagePokemon
+// inputElement.value = ""
+
+//     }
+//     catch(error){
+// console.error(error)
+//     }
+//   }
+
+
+//   function CliquerPokemon(){
+//     let bouton = document.getElementById('monBouton');
+//     bouton.addEventListener('click',ChercherPokemon)
+//   }
+ 
+//   CliquerPokemon()
+
+
+  
+
