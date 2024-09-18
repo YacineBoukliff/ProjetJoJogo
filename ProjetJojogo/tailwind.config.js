4d75f49a8ce37597243d07b346b132d22334861b
yacine bouklif <y.bouklif@it-students.fr>
2024-09-18 16:00:49 +0200

    Fix de tailwind

--- ProjetJojogo/tailwind.config.js
+++ ProjetJojogo/tailwind.config.js
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './Html/**/*.html',  // Cible tous les fichiers HTML dans le dossier Html et ses sous-dossiers
-    './**/*.html', 
+    './Html/**/*.html',
+    './**/*.html',
   ],
   theme: {
     extend: {
+      colors: {
+        'custom-blue': '#1e40af',  // Ajoutez votre couleur personnalisée ici
+      },
       backgroundImage: {
         radialgradient: 'radial-gradient(circle at 50% 92%, rgba(15, 20, 218, 1), rgba(193, 185, 186, 1))',
       },
@@ -15,7 +18,7 @@ module.exports = {
     require('daisyui'),
   ],
   daisyui: {
-    themes: [ "dark", "light" ],
+    themes: ["dark", "light"],
   },
-  darkMode: 'class',  
+  darkMode: 'class',
 }
